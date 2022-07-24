7ed043a924c635e415b779073e516a3bcbe28c9c
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-07-24 17:59:26 +1000

    Move structs

--- src/rejections.rs
+++ src/rejections.rs
@@ -3,9 +3,8 @@ use warp::reject;
 #[derive(Debug)]
 pub struct NoContentProvided;
 
-impl reject::Reject for NoContentProvided {}
-
 #[derive(Debug)]
 pub struct NoValue;
 
+impl reject::Reject for NoContentProvided {}
 impl reject::Reject for NoValue {}
